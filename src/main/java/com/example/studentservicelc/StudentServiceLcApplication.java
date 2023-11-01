991ec1ba38d7a8a3655a7539a14d6ee9a01a5417
theali <thealiergashev@gmail.com>
2023-11-01 21:26:43 +0500

    Initial commit

---
+++ src/main/java/com/example/studentservicelc/StudentServiceLcApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentservicelc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceLcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentServiceLcApplication.class, args);
+    }
+
+}
